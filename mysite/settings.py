5437105e3e725f0c1496bd6c14b35e04227a6795
Propupul <luisvalencia3@hotmail.com>
2017-07-12 13:54:52 -0400

    Added mysql connection to pythonanywhere

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'propupul$blog',
+        'USER': 'propupul',
+        'PASSWORD': 'D3wBw9va=8Pvqmgn6YZbW&)E[oy39s7$g7LpEA@vpkRx$Z8kMKBy,jDCn;.dM8YD',
+        'HOST': 'propupul.mysql.pythonanywhere-services.com',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
